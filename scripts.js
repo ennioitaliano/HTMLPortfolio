015646d169431f1d137a73e5b067ebd27eebdb99
Ennio Italiano <ennio.italiano@gmail.com>
2022-09-05 16:50:59 +0200

    Little more elegant solution for dark mode

--- scripts.js
+++ scripts.js
@@ -1,52 +1,22 @@
-var toggle = document.getElementById("darkmode");
-var memoji = document.getElementById("memoji");
-var body = document.body;
-var iconshf = document.getElementsByClassName("fahf");
-var iconsw = document.getElementsByClassName("faw");
-var workstitles = document.getElementsByClassName("tl");
+// Select the button
+const darkmodeswitch = document.getElementById("darkmode");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+const memoji = document.getElementById("memoji");
 
-function darkmode() {
-    if (toggle.textContent == "Dark mode") {
-        memoji.setAttribute("src", "./media/memojisleep.png");
-        body.classList.toggle("dark-mode");
-
-        for (let i = 0; i < iconshf.length; i++) {
-            iconshf[i].classList.toggle("dark-mode");
-        }
-
-        for (let i = 0; i < iconsw.length; i++) {
-            iconsw[i].classList.toggle("dark-mode-faw");
-        }
-
-        for (let i = 0; i < workstitles.length; i++) {
-            workstitles[i].classList.toggle("dark-mode-titles");
-            workstitles[i].classList.toggle("titlelink");
-        }
-
-        toggle.classList.toggle("dark-mode-toggle");
-        toggle.classList.toggle("darkmodetext");
-        toggle.classList.toggle("link");
-        toggle.innerHTML = "Light mode";
-    } else {
-        memoji.setAttribute("src", "./media/memoji.png");
-        body.classList.toggle("dark-mode");
-
-        for (let i = 0; i < iconshf.length; i++) {
-            iconshf[i].classList.toggle("dark-mode");
-        }
-
-        for (let i = 0; i < iconsw.length; i++) {
-            iconsw[i].classList.toggle("dark-mode-faw");
-        }
-
-        for (let i = 0; i < workstitles.length; i++) {
-            workstitles[i].classList.toggle("dark-mode-titles");
-            workstitles[i].classList.toggle("titlelink");
-        }
-
-        toggle.classList.toggle("dark-mode-toggle");
-        toggle.classList.toggle("darkmodetext");
-        toggle.classList.toggle("link");
-        toggle.innerHTML = "Dark mode";
-    }
-}
+// Listen for a click on the button
+darkmodeswitch.addEventListener("click", function() {
+  // If the current URL contains "ligh.css"
+  if (theme.getAttribute("href") == "light.css") {
+    memoji.setAttribute("src", "./media/memojisleep.png");
+    // ... then switch it to "dark-theme.css"
+    theme.href = "dark.css";
+    darkmodeswitch.innerHTML = "Light mode";
+  // Otherwise...
+  } else {
+    memoji.setAttribute("src", "./media/memoji.png");
+    // ... switch it to "light-theme.css"
+    theme.href = "light.css";
+    darkmodeswitch.innerHTML = "Dark mode";
+  }
+});
