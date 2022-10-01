e87839dc8d6761aa2ffda7fee91337f9d8f9e1bb
ennioitaliano <ennio.italiano@gmail.com>
2022-10-01 13:10:07 +0200

    Started refactoring

--- scripts.js
+++ scripts.js
@@ -11,12 +11,17 @@ darkmodeswitch.addEventListener("click", function() {
     memoji.setAttribute("src", "./media/memojisleep.png");
     // ... then switch it to "dark-theme.css"
     theme.href = "dark.css";
-    darkmodeswitch.innerHTML = "Light mode";
+    darkmodeswitch.innerHTML = "Light Mode";
   // Otherwise...
   } else {
     memoji.setAttribute("src", "./media/memoji.png");
     // ... switch it to "light-theme.css"
     theme.href = "light.css";
-    darkmodeswitch.innerHTML = "Dark mode";
+    darkmodeswitch.innerHTML = "Dark Mode";
   }
 });
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
