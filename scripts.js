2b6461989c522e76382e18e32e37f5f53cb2fbb4
ennioitaliano <ennio.italiano@gmail.com>
2022-10-04 09:34:29 +0200

    Updated dark mode

--- scripts.js
+++ scripts.js
@@ -1,25 +1,29 @@
-// Select the button
-const darkmodeswitch = document.getElementById("darkmode");
-// Select the stylesheet <link>
-const theme = document.querySelector("#theme-link");
+const darkModeButton = document.getElementById("darkmode");
 const memoji = document.getElementById("memoji");
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-// Listen for a click on the button
-darkmodeswitch.addEventListener("click", function() {
-  // If the current URL contains "ligh.css"
-  if (theme.getAttribute("href") == "light.css") {
-    memoji.setAttribute("src", "./media/memojisleep.png");
-    // ... then switch it to "dark-theme.css"
-    theme.href = "dark.css";
-    darkmodeswitch.innerHTML = "Light Mode";
-  // Otherwise...
-  } else {
-    memoji.setAttribute("src", "./media/memoji.png");
-    // ... switch it to "light-theme.css"
-    theme.href = "light.css";
-    darkmodeswitch.innerHTML = "Dark Mode";
-  }
-});
+function darkModeSwitcher() {
+    if (document.documentElement.classList.contains("dark-mode")) {
+        memoji.setAttribute("src", "./media/memoji.png");
+        document.documentElement.classList.toggle("dark-mode");
+        darkModeButton.innerHTML = "Dark Mode";
+    } else {
+        memoji.setAttribute("src", "./media/memojisleep.png");
+        document.documentElement.classList.toggle("dark-mode");
+        darkModeButton.innerHTML = "Light Mode";
+    }
+}
+
+function toggleDarkMode(state) {
+    //document.documentElement.classList.toggle("dark-mode", state);
+    darkModeSwitcher();
+}
+
+// Listening for the changes in the OS settings and auto switching the mode
+useDark.addListener((evt) => toggleDarkMode(evt.matches));
+
+// Toggles the "dark-mode" class on click
+darkModeButton.addEventListener("click", () => darkModeSwitcher());
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
